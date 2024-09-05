0ae5b4f0f4b8a28455b66859f37005a28b93364b
Abhinav Gupta <ag0537269@gmail.com>
2024-09-05 22:26:11 +0530

    hashmap

---
+++ src/main/java/com/interview/hashmap/MainExample.java
@@ -0,0 +1,12 @@
+package com.interview.hashmap;
+
+import java.util.HashMap;
+
+public class MainExample {
+    public static void main(String[] args) {
+        HashMap<String, Integer> friends = new HashMap<>();
+        friends.put("Abhinav", 20);
+        friends.put("Ankit", 25);
+        System.out.println(friends);
+    }
+}
